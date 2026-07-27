ed920a2c14e7754737dd0cdcf2a403025d57d9f8
Yogesh R <yogeshraj.btech@gmail.com>
2026-07-27 14:53:38 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Array Leaders/array-leaders.java
@@ -0,0 +1,24 @@
+class Solution {
+    static ArrayList<Integer> leaders(int arr[]) {
+        // code here
+        ArrayList <Integer> obj=new ArrayList<>();
+        obj.add(arr[arr.length-1]);
+        int max=arr[arr.length-1];
+        for(int i=arr.length-2;i>=0;i--){
+            if(arr[i]>=max){
+                obj.add(arr[i]);
+                max=arr[i];
+            }
+        }
+        int l=0;
+        int r=obj.size()-1;
+        while(l<r){
+            int temp=obj.get(l);
+            obj.set(l,obj.get(r));
+            obj.set(r,temp);
+            l++;
+            r--;
+        }
+        return obj;
+    }
+}
