f64332f736d3a610c693935fc76be1fcad786b1a
Yogesh Rajkumar <yogeshraj.btech@gmail.com>
2026-08-08 21:21:18 +0530

    Update rearrange-array-alternately.java

--- Difficulty: Medium/Rearrange Array Alternately/rearrange-array-alternately.java
+++ Difficulty: Medium/Rearrange Array Alternately/rearrange-array-alternately.java
@@ -19,3 +19,27 @@ class Solution {
         }
     }
 }
+
+// base is always the max+1 in an array
+
+// encryption = min+(max*base)
+
+// here,
+// encryption = min+(max*base)%base gives min
+// encryption = min+(min*base)/base gives max 
+
+// ex:
+// 1 2 3 4 5
+// 1+(5*6)=1+30
+//        =31
+    
+//     min=31%6=1
+//     max=31/6=5
+
+// arr[r]%base for decrypte the already encoded number by you
+
+// arr[i]=arr[i]+(arr[r]%base)*base;
+// arr[r] is what you are going to encrypting - max
+
+// arr[i]=arr[i]+(arr[l]%base)*base;
+// similarly, arr[l] - min
