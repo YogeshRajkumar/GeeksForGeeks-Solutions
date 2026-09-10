ff727fb041ec2d1ea86a1f5b3a52abca9a374b41
Yogesh Rajkumar <yogeshraj.btech@gmail.com>
2026-09-10 11:03:15 +0530

    Update Larget-subarray-with-0-sum.java

--- Difficulty: Medium/Largest subarray with 0 sum/Larget-subarray-with-0-sum.java
+++ Difficulty: Medium/Largest subarray with 0 sum/Larget-subarray-with-0-sum.java
@@ -1,1 +1,20 @@
-
+class Solution {
+    int maxLength(int arr[]) {
+        HashMap<Integer,Integer> obj=new HashMap<>();
+        obj.put(0,-1);
+        int r=0;
+        int sum=0;
+        int max=0;
+        while(r<arr.length){
+            sum+=arr[r];
+            if(obj.containsKey(sum)){
+                max=Math.max(max,r-obj.get(sum));
+            }
+            else{
+                obj.put(sum,r);
+            }
+            r++;
+        }
+        return max;
+    }
+}
